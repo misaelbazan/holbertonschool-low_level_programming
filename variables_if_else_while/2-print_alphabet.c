646f590ad38bd1d37ad3b35f0bc09ffddf64bcd4
misaelbazan <misaelbfranco@gmail.com>
2023-08-06 21:00:51 -0700

    c program that putchar the alphabet, adding a new line

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	/* Declare variables*/
+	char alphabet;
+
+	/* Code starts here*/
+	for (alphabet = 'a'; alphabet <= 'z'; ++alphabet)
+	{
+		putchar(alphabet);
+		putchar('\n');
+	}
+
+	return (0);
+}
