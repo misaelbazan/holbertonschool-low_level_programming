4979215c37f918ec5005d41f64f30345e91ba09c
misaelbazan <misaelbfranco@gmail.com>
2023-08-12 21:25:38 -0700

    Code that print the absolute value of an integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - function
+ *
+ * @n: integer to be evaluated
+ *
+ * Return: Always the absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+
+	else
+	{
+		return (-n);
+	}
+}
---
+++ functions_nested_loops/6-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the absolute value of a integer
+ *
+ * @r: variable to be evaluated
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int r;
+
+	r = _abs(-1);
+	printf("%d\n", r);
+	r = _abs(0);
+	printf("%d\n", r);
+	r = _abs(1);
+	printf("%d\n", r);
+	r = _abs(-98);
+	printf("%d\n", r);
+	return (0);
+}
