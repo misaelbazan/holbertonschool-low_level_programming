7256301b9098435003c4e5fa6b5d27c0da7d1af0
misaelbazan <you@example.com>
2023-08-17 05:11:00 -0700

    Write a function that swaps the values of two integers.

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Function that changes the values
+ *
+ * @a: Pointer that is deferencing a
+ * @b: Pointer deference b
+ *
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
