2242fbfe5c48e89cf570959260d4b4bbd4c0f262
misaelbazan <you@example.com>
2023-08-19 15:31:17 -0700

    Solved a medias

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _atoi - converts any string into numbers
+ * if there's no numbers, return 0
+ *
+ * @s: string to be converted
+ *
+ * Return: Always val
+ */
+int _atoi(char *s)
+{
+	int val;
+
+	val = atoi(s);
+	return (val);
+}
