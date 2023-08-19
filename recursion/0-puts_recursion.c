f916d5248986921b9caaf1a5399091a5e0eb7c07
misaelbazan <you@example.com>
2023-08-19 16:02:52 -0700

    Write a function that prints a string, followed by a new line.

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that puts on screen the string given
+ *
+ * @s: String given
+ *
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ recursion/README.md
@@ -0,0 +1,2 @@
+0-puts_recursion.c
+
