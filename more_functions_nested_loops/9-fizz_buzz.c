bef1794844a2b89e4c7bc0a119981608f95883c8
misaelbazan <you@example.com>
2023-08-18 17:10:27 -0700

    Solved: Space after

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,19 @@ int main(void)
 	{
 		if (contador % 3 == 0 && contador % 5 != 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if (contador % 3 != 0 && contador % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (contador % 3 == 0 && contador % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", contador);
+			printf(" %d", contador);
 		}
 	}
 	printf("\n");
