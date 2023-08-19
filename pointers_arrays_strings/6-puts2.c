4b2bb041783fb30d385562cedcb340ba5397bdc6
misaelbazan <you@example.com>
2023-08-18 21:03:48 -0700

    Push

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,21 @@
-#include
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - function that prints on screen
+ *
+ * @str: string that would be avaluated
+ *
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < (int)strlen(str); i += 2)
+	{
+		printf("%c", str[i]);
+	}
+	printf("\n");
+}
